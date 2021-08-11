5d5b99d375c8f732e3c032dbd14b1b5a57eeb766
Anjit Pariyar <anjitpariyar@gmail.com>
2021-08-11 18:36:22 +0545

    second commit

---
+++ src/Components/Title.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./title.module.css";
+
+const Title = ({ text, arc, radius, className }) => {
+  const characters = text.split("");
+  const degree = arc / characters.length;
+
+  return (
+    <h2
+      className={styles.Title + " " + className}
+      style={{ height: `${radius / 1.5}px` }}
+    >
+      {characters.map((char, i) => (
+        <span
+          key={`heading-span-${i}`}
+          style={{
+            height: `${radius}px`,
+            transform: `rotate(${degree * i - arc / 2}deg)`,
+            transformOrigin: `0 ${radius}px 0`,
+            position: "absolute",
+          }}
+        >
+          {char}
+        </span>
+      ))}
+    </h2>
+  );
+};
+
+Title.propTypes = {
+  text: PropTypes.string.isRequired,
+  arc: PropTypes.number, // how curved do you want the text
+  radius: PropTypes.number, // how big do you want the curve
+};
+
+Title.defaultProps = {
+  arc: 120,
+  radius: 400,
+  text: "Test",
+  className: "test",
+};
+
+export default Title;
